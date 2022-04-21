3787d9055585eeba419c849bd501018fc0a7980f
Silver Chen <SilverChen@microsoft.com>
2022-04-22 09:56:26 +1000

    feat: initial commit

---
+++ test/command.ps1
@@ -0,0 +1,9 @@
+az deployment group create --name demoRGDeployment --resource-group ExampleGroup  --template-uri "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/quickstarts/microsoft.storage/storage-account-create/azuredeploy.json"  --parameters storageAccountType=Standard_GRS
+
+
+az vm create `
+    --resource-group IaaSExample-rg01 `
+    --name myVM `
+    --image Win2019Datacenter `
+    --public-ip-sku Standard `
+    --admin-username azureuser
